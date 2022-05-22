f1e8dfad28727ebe55e1f5547844536dac929794
VahagnVardanyan <vahagvardanyan99@mail.ru>
2022-05-22 21:11:00 +0400

    homepage

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,11 @@
+import classes from "./Footer.module.css"
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={classes.footer}>
+      <div>Movie App</div>
+      <div>2022, Movie , inc, or its affiliates</div>
+
+    </div>
   )
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+
+import MovieListing from '../MovieListing/MovieListing'
+
+function Home({movie}) {
+ 
+  return (
+      <div>
+    <MovieListing movie ={movie}/>
+    </div>
+    
+  )
+}
+
+export default Home
