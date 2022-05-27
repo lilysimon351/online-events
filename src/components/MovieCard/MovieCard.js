aa576583259375640dd2a6140c5a46e294844794
Yegor97 <yegorkarapetyan1997@gmail.com>
2022-05-28 00:00:03 +0400

    add login register header + buttun to favorites

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom';
+import { useUserInfo } from '../../context/UserProvider';
 import classes from "./MovieCard.module.css"
 
 function MovieCard({movie}) {
+	const {user} = useUserInfo()
 
   return ( 
 	<Link to={`/movies/${movie.id}`} key={movie.id}>
@@ -10,7 +12,10 @@ function MovieCard({movie}) {
 			<img className={classes.image} src={movie.url} alt={movie.title} />
 			<h2>{movie.title} {movie.date}</h2>
 			<h2>{movie.count}</h2>
-			<button className={classes.like}>To favorites</button> 
+			{
+			 user && <button className={classes.like}>To favorites</button> 
+			}
+			
 		</div>
 	</Link>
   )
