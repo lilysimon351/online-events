ecfe154092c2c23611643c88ce751e1362dc3f40
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-29 01:23:33 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import RouteProvider from './context/RouteProvider';
+import UserProvider from './context/UserProvider';
+import LanguageProvider from "./context/LanguageProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <LanguageProvider>
+      <RouteProvider>
+        <UserProvider>
+          <App />
+        </UserProvider> 
+      </RouteProvider>
+    </LanguageProvider>
   </BrowserRouter>
 );
