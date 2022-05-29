53b88bcb2f87ca9bb9a2aeb9287e07d0f49384a2
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-29 23:54:28 +0400

    c

--- src/App.js
+++ src/App.js
@@ -12,33 +12,26 @@ import AddMovie from './components/Admin/AddMovie';
 import AdminMovies from './components/Admin/AdminMovies.js';
 import EditMovie from './components/Admin/EditMovie';
 import AuthComponent from './components/Auth/AuthComponent';
-<<<<<<< HEAD
-
-
 import FavMovie from './components/FavMovie/FavMovie';
-<<<<<<< HEAD
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
 
 
->>>>>>> 37cab2f6efd4541544440f5609a6c601646aa252
 function App() {
   return (
     <div className='app'>
 		<Header/>
 		<Routes>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 			<Route path='home' element={<Home/>}/> 
 			<Route path='aboutUs' element ={<AboutUs/>}/>
 			<Route path='auth' element ={<AuthComponent/>}/>
 			<Route path='movies/:id' element={<MovieDetail />} />
 			<Route path="*" element={<Navigate to ='home' /> }/>
-=======
+
 			<Route path="/home" element={<Home/>}/> 
-=======
+
 			<Route path="/home" element={<Home/>} />
 			<Route path="home/:id" element={<MovieDetail />} />
->>>>>>> 37cab2f6efd4541544440f5609a6c601646aa252
+
 			<Route path='aboutUs' element ={<AboutUs/>}/>
 			<Route path='auth' element ={<AuthComponent/>}/>
 			<Route path="favorite" element={<FavMovie/>} />
@@ -51,7 +44,7 @@ function App() {
 			
 			<Route path="*" element={<PageNotFound /> }/>
      
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
+
 		</Routes>
 		<Footer/>
       
