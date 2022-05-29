32b15245b54b97c88379d40264055b141b61e2e7
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-30 00:02:17 +0400

    c

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -7,12 +7,7 @@ const AboutUs = () => {
 
   return (
     <div>
-<<<<<<< HEAD
         <h1>Aystex karox e linel dzer govazdy</h1>
-=======
-    
-      <h1>Aystex karox e linel dzer govazdy</h1>
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
     </div>
   )
 }
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,4 @@
 export const HEADER_LINKS = [
-<<<<<<< HEAD
   {
     id:'1',
     to:'home',
@@ -29,30 +28,3 @@ export const ACTIVE_ROUTES =[
 
 // ]
 export const LANGUAGES = ['AM', 'RU', 'EN']
-=======
-    {
-      id:'1',
-      to:'home',
-      title:'MOVIES'
-    },
-    {
-      id:'2',
-      to:'aboutUs',
-      title:'ABOUT US'
-    },
-    {
-        id:'3',
-        to:'auth',
-        title:'AUTH'
-    },
-    {
-      id:"4",
-      to:"favorite",
-      title:"FAVORITES MOVIES"
-    }
-  ] 
-  export const ACTIVE_ROUTES =[
-      'LOGIN',
-      'REGISTER'
-  ]
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
