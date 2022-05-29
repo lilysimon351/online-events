c1646442e5dd33c57b3aaea906ed71ffb5e1d826
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-29 10:21:47 +0400

    ց

--- src/helpers/translations.js
+++ src/helpers/translations.js
@@ -5,6 +5,7 @@ export const EN = {
     logOut: 'LOG OUT',
     login: 'LOGIN',
     password: 'PASSWORD',
+    'Aystex karox e linel dzer govazdy':'Aystex karox e linel dzer govazdy',
     armenian: 'Armenian',
     russian: 'Russian',
     english: 'English',
@@ -16,6 +17,7 @@ export const RU = {
     logOut: 'ВЫЙТИ',
     login: 'ЛОГИН',
     password: 'ПАРОЛЬ',
+    'Aystex karox e linel dzer govazdy':'Здесь может быть Ваша реклама',
     armenian: 'Армянский',
     russian: 'Русский',
     english: 'Английский'
@@ -27,6 +29,7 @@ export const AM = {
     logOut: 'ԴՈՒՐՍ ԳԱԼ',
     login: 'ՄՈՒՏՔԱՆՈՒՆ',
     password: 'ԳԱՂՏՆԱԲԱՌ',
+    'Aystex karox e linel dzer govazdy':'Այստեղ կարող է լինել Ձեր գոցազդը',
     armenian: 'Հայերեն',
     russian: 'Ռուսերեն',
     english: 'Անգլերեն'
