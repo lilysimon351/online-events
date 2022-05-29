d4e6b666dd9515d2f1aff0ab68f88b1409832ad0
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-29 15:08:28 +0400

    c

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes,Route,Navigate } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import Header from "./components/Header/Header"
+import Header from './components/Header/Header';
 import AboutUs from './components/AboutUs/AboutUs';
 import PageNotFound from './components/PageNotFound/PageNotFound';
 import MovieDetail from './components/MovieDetail/MovieDetail';
