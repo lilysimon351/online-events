61df4769ed0674223777fb4c823ce6c909976d8f
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2022-05-29 19:57:12 +0400

    clear admin.js file from unused code

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,41 +4,7 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { Outlet, useNavigate } from "react-router-dom";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
-
 export default function Admin() {
-
-	const navigate = useNavigate();
-	const goToAdmin = () => navigate('/admin')
-	const goToAdminUsers = () => navigate('/admin/users')
-
-
-	const [value, setValue] = useState(0);
-
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-	};
 
 	return (
 		<div style={{background: 'white',padding: '30px'}}>
