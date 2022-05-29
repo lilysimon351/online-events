6bea127f05e7165336651855a731d96941e3af65
Roberta-Yelena <79405785+Roberta-Yelena@users.noreply.github.com>
2022-05-29 23:39:04 +0400

    c

--- src/index.js
+++ src/index.js
@@ -5,46 +5,37 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import RouteProvider from './context/RouteProvider';
 import UserProvider from './context/UserProvider';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import LanguageProvider from "./context/LanguageProvider";
-=======
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
-=======
 import { Provider } from 'react-redux';
 import { store } from './store/store';
->>>>>>> 37cab2f6efd4541544440f5609a6c601646aa252
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-<<<<<<< HEAD
-  <BrowserRouter>
-<<<<<<< HEAD
+
+  <><BrowserRouter>
     <LanguageProvider>
       <RouteProvider>
         <UserProvider>
           <App />
-        </UserProvider> 
+        </UserProvider>
       </RouteProvider>
     </LanguageProvider>
-=======
+
     <UserProvider>
       <RouteProvider>
         <App />
       </RouteProvider>
-    </UserProvider> 
->>>>>>> 9beae30245ce5f80c8dae34eef28bd181c2fe80d
-  </BrowserRouter>
-=======
-  <Provider store={store}>
-    <BrowserRouter>
-      <UserProvider>
-        <RouteProvider>
-          <App />
-        </RouteProvider>
-      </UserProvider> 
-    </BrowserRouter>
-  </Provider>
->>>>>>> 37cab2f6efd4541544440f5609a6c601646aa252
+    </UserProvider>
+
+  </BrowserRouter><Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <RouteProvider>
+            <App />
+          </RouteProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider></>
 );
